6350c23d849695e044f036b0776b2581ef53b75e
LilithDaly <contact@lilithdaly.com>
2022-03-28 06:57:40 -0400

    aaaaaaaaaa

--- public/index.js
+++ public/index.js
@@ -1,59 +1,44 @@
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
 import { FBXLoader } from "https://unpkg.com/three@0.127.0/examples/jsm/loaders/FBXLoader.js"
 
+const PI = 3.14159
+
+let model
+
 // Create Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x282c34);
 
-// Define a camera, set it to fill the browser window and position it
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.x = 20.2469;
-camera.position.y = -8.64454;
-camera.position.z = 6.37;
-
-camera.rotation.x = 72.4;
-camera.rotation.y = 0;
-camera.rotation.z = 90;
-
-// Define a renderer, and set it to fill the browser window
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Get an element from the DOM and append renderer.domElement to it
+renderer.setPixelRatio(window.devicePixelRatio);
 document.getElementById('threejs').appendChild(renderer.domElement);
 
-// Define (or import) your object's geometry
-// const geometry = new THREE.BoxGeometry( 20, 20, 20 );
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(-9, 4, 14);
+camera.rotation.set(-0.06 * PI, 0, -0.015 * PI);
 
-
-// Define your object's material
-// const material = new THREE.MeshStandardMaterial({
-//   color: 0xfcc742,
-//   emissive: 0x111111,
-//   metalness: 0,
-//   roughness: 1,
+// const loader = new ColladaLoader();
+// loader.load("Models/shep.dae", (collada) => {
+//     model = collada.scene;
+//     model.updateMatrix();
+//     scene.add(model);
+// }, undefined, (e) => {
+//     console.error(e);
 // });
 
-// Create the mesh, scale it and add it to the scene
-// const mesh = new THREE.Mesh(geometry, material);
-
-// mesh.scale.x = 0.1;
-// mesh.scale.y = 0.1;
-// mesh.scale.z = 0.1;
-
-const mesh = new THREE.Mesh()
 const loader = new FBXLoader();
 loader.load("shep.fbx", 
     (obj) => {
-        obj.rotation.x = 94.3;
-        obj.rotation.y = 0;
-        obj.rotation.z = 181;
-
-        obj.scale.x = 0.01;
-        obj.scale.y = 0.01;
-        obj.scale.z = 0.01;
-
+        obj.scale.set(0.01, 0.01, 0.01);
+        obj.rotation.set(0.01 * PI, -0.315 * PI, 0.02 * PI);
         scene.add(obj)
     },
     (xhr) => {
@@ -64,7 +49,6 @@ loader.load("shep.fbx",
     }
 );
 
-// Create lights, position them, and add them to the scene
 const frontSpot = new THREE.SpotLight(0xeeeece);
 const frontSpot2 = new THREE.SpotLight(0xddddce);
 
@@ -74,12 +58,10 @@ frontSpot2.position.set(-500, -500, -500);
 scene.add(frontSpot);
 scene.add(frontSpot2);
 
-// Create an animate function, which will allow you to render your scene and define any movements
 const animate = function () {
   requestAnimationFrame(animate);
 
   renderer.render(scene, camera);
 };
 
-// Call the animate function
 animate();
