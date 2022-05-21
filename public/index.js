90fe551b0d8a7ca5bd4a6fa1a12a00fe6ed15a15
LilithDaly <contact@lilithdaly.com>
2022-05-21 19:01:47 -0400

    yep, still work

--- public/index.js
+++ public/index.js
@@ -1,42 +1,47 @@
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
-import { FBXLoader } from "https://unpkg.com/three@0.127.0/examples/jsm/loaders/FBXLoader.js"
+import { GLTFLoader } from "https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js"
 
-let model
+let t = 0
 
 // Create Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x282c34);
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
+let size = get_size();
 renderer.setPixelRatio(window.devicePixelRatio);
-var size = get_size()
-renderer.setSize(size);
+renderer.setSize(size * 1.5, size);
 renderer.setPixelRatio(window.devicePixelRatio);
-const renderDom = renderer.domElement
-renderDom.width = size
-renderDom.height = size
+renderer.domElement.id = "WebGl";
 
 const three = document.getElementById('threejs')
 three.appendChild(renderer.domElement);
 
 window.addEventListener('resize', () => {
-    size = get_size()
-    renderer.setSize(size);
-    renderDom.width = size
-    renderDom.height = size
+    size = get_size();
+    renderer.setSize(size * 1.5, size);
+    renderer.setPixelRatio(window.devicePixelRatio);
 }, false);
 
 function get_size() {
-    var minLength = Math.min(window.innerWidth, window.innerHeight)
-    console.log(minLength)
-    return minLength * 0.8
+    var minLength = Math.min(window.innerWidth / 1.5, window.innerHeight);
+    return minLength * 0.8;
 }
 
-const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-camera.position.set(-9, 2, 14);
-camera.rotation.set(-0.01 * Math.PI, 0, -0.015 * Math.PI);
+const camera = new THREE.PerspectiveCamera(50, 1.5, 0.1, 1000);
+camera.position.set(-2, 4, 15);
+camera.rotation.set(-0.08 * Math.PI, 0, 0);
 
-// const loader = new ColladaLoader();
+const material = new THREE.MeshToonMaterial()
+material.gradientMap = null;
+
+const loader = new GLTFLoader();
+loader.load("shep.glb", (gltf) => {
+    const shepMesh = gltf.scene.children()[0];
+    shepMesh.material = material
+}, undefined, (e) => {
+    console.error(e);
+});
 // loader.load("Models/shep.dae", (collada) => {
 //     model = collada.scene;
 //     model.updateMatrix();
@@ -45,20 +50,23 @@ camera.rotation.set(-0.01 * Math.PI, 0, -0.015 * Math.PI);
 //     console.error(e);
 // });
 
-const loader = new FBXLoader();
-loader.load("shep.fbx", 
-    (obj) => {
-        obj.scale.set(0.01, 0.01, 0.01);
-        obj.rotation.set(0.01 * Math.PI, -0.315 * Math.PI, 0.02 * Math.PI);
-        scene.add(obj)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-);
+// const loader = new FBXLoader();
+
+// loader.load("shep.fbx", 
+//     (obj) => {
+//         model=obj;
+//         model.scale.set(0.01, 0.01, 0.01);
+//         model.rotation.set(0.01 * Math.PI, -0.315 * Math.PI, 0.02 * Math.PI);
+//         model.material = material
+//         scene.add(model)
+//     },
+//     (xhr) => {
+//         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+//     },
+//     (error) => {
+//         console.log(error)
+//     }
+// );
 
 const frontSpot = new THREE.SpotLight(0xeeeece);
 const frontSpot2 = new THREE.SpotLight(0xddddce);
@@ -70,9 +78,14 @@ scene.add(frontSpot);
 scene.add(frontSpot2);
 
 const animate = function () {
-  requestAnimationFrame(animate);
-
-  renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    
+    // if (model) {
+    //     model.rotation.set((0.01 + (t * 0.001)) * Math.PI, -0.315 * Math.PI, (0.02 + (t * 0.001)) * Math.PI);
+    // }
+    
+    t++;
+    renderer.render(scene, camera);
 };
 
 animate();
