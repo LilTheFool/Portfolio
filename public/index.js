a5847b5ce1b23b4f5a54801c4f71415e25e72084
LilithDaly <contact@lilithdaly.com>
2022-05-31 04:27:39 -0400

    aaaaa

--- public/index.js
+++ public/index.js
@@ -1,72 +1,64 @@
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
 import { GLTFLoader } from "https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js"
 
-let t = 0
+let t = 0;
+let shepParts = [];
 
-// Create Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x282c34);
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+
 let size = get_size();
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(size * 1.5, size);
+renderer.setClearColor(0x000000, 0);
+renderer.setSize(size * 1.25, size);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.domElement.id = "WebGl";
 
-const three = document.getElementById('threejs')
+const three = document.getElementById('threejs');
 three.appendChild(renderer.domElement);
+
+const dialogue = document.querySelector("#dialogue");
+dialogue.style.right = size + "px";
 
 window.addEventListener('resize', () => {
     size = get_size();
-    renderer.setSize(size * 1.5, size);
+    renderer.setSize(size * 1.25, size);
     renderer.setPixelRatio(window.devicePixelRatio);
+    dialogue.style.right = size + "px";
 }, false);
 
 function get_size() {
-    var minLength = Math.min(window.innerWidth / 1.5, window.innerHeight);
-    return minLength * 0.8;
+    var minLength = Math.min(window.innerWidth / 1.25, window.innerHeight);
+    return minLength * 0.6;
 }
 
-const camera = new THREE.PerspectiveCamera(50, 1.5, 0.1, 1000);
-camera.position.set(-2, 4, 15);
-camera.rotation.set(-0.08 * Math.PI, 0, 0);
+const camera = new THREE.PerspectiveCamera(36, 1.25, 0.1, 1000);
+camera.position.set(-0.25,0,-21);
+camera.rotation.set(0, Math.PI, 0);
 
-const material = new THREE.MeshToonMaterial()
-material.gradientMap = null;
+const material1 = new THREE.MeshToonMaterial()
+material1.color = new THREE.Color(0x959490);
+
+const material2 = new THREE.MeshToonMaterial()
+material2.color = new THREE.Color(0x353535);
 
 const loader = new GLTFLoader();
 loader.load("shep.glb", (gltf) => {
-    const shepMesh = gltf.scene.children()[0];
-    shepMesh.material = material
+    const shep = gltf.scene.children[0];
+
+    const child0 = shep.children[0];
+    child0.material = material1;
+    scene.add(child0);
+    
+    const child1 = shep.children[0];
+    child1.material = material2;
+    scene.add(child1);
+
+    shepParts.push(child0, child1);
+
 }, undefined, (e) => {
     console.error(e);
 });
-// loader.load("Models/shep.dae", (collada) => {
-//     model = collada.scene;
-//     model.updateMatrix();
-//     scene.add(model);
-// }, undefined, (e) => {
-//     console.error(e);
-// });
-
-// const loader = new FBXLoader();
-
-// loader.load("shep.fbx", 
-//     (obj) => {
-//         model=obj;
-//         model.scale.set(0.01, 0.01, 0.01);
-//         model.rotation.set(0.01 * Math.PI, -0.315 * Math.PI, 0.02 * Math.PI);
-//         model.material = material
-//         scene.add(model)
-//     },
-//     (xhr) => {
-//         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-//     },
-//     (error) => {
-//         console.log(error)
-//     }
-// );
 
 const frontSpot = new THREE.SpotLight(0xeeeece);
 const frontSpot2 = new THREE.SpotLight(0xddddce);
@@ -79,10 +71,10 @@ scene.add(frontSpot2);
 
 const animate = function () {
     requestAnimationFrame(animate);
-    
-    // if (model) {
-    //     model.rotation.set((0.01 + (t * 0.001)) * Math.PI, -0.315 * Math.PI, (0.02 + (t * 0.001)) * Math.PI);
-    // }
+
+    shepParts.forEach((e) => {
+        e.rotation.set(0, t / 100, t / 200)
+    });
     
     t++;
     renderer.render(scene, camera);
